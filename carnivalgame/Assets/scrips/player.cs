62f8a14fd208f4adce36d5e77ced10eab0f443e4
Amoorer127 <Amoorer95@gmail.com>
2017-05-17 20:31:10 -0400

    carnival

---
+++ carnivalgame/Assets/scrips/player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    public float speed;
+    //public GameObject ball;
+    public GameObject[] targets;
+    private Rigidbody rb;
+    private Rigidbody projectile;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+       // projectile = ball.GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+           rb.velocity = (transform.right *-speed);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = (transform.right *speed);
+        }
+
+        for (int i = 0; i < targets.Length; i++)
+        {
+            //so long as the enemy is not past the rightmost position on the board (3), move them by 0.2 per frame
+            if (targets[i].transform.position.x < 29)
+            {
+                targets[i].transform.position += new Vector3(.2f, 0, 0);
+            }
+            else
+            {
+                targets[i].transform.position = new Vector3(0, targets[i].transform.position.y, targets[i].transform.position.z);
+            }
+        }
+
+
+
+
+            //if the player is in the same position as the enemy, reset them and the key
+
+            // float moveHorizontal = Input.GetAxis("Horizontal");
+
+            // Vector3 movement = new Vector3(moveHorizontal, 0, 0);
+
+            // if (Input.GetMouseButtonDown(0))
+            //{
+            //ball.transform.position = transform.position + Camera.main.transform.forward * 2;
+            //  projectile.velocity = Camera.main.transform.forward * 40; 
+
+            //}
+        }
+
+
+}
---
+++ carnivalgame/Assets/scrips/progectile.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class progectile : MonoBehaviour {
+    //GameObject projectile;
+    public Renderer rend;
+    GameObject prefab;
+    public AudioSource shoot;
+	// Use this for initialization
+	void Start () {
+        prefab = Resources.Load("sphere") as GameObject;
+        // projectile = Instantiate(prefab) as GameObject;
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
+        { 
+           GameObject projectile = Instantiate(prefab) as GameObject;
+           projectile.transform.position = transform.position + Camera.main.transform.forward * 2;
+
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            rb.velocity = Camera.main.transform.forward * 40;
+
+            shoot.Play();
+        }
+	}
+   // void OnCollisionEnter(Collision colReport)
+    //{
+        //only do something if the collision report says the object collided with was a...
+        //game object with a tag property of "duckling"
+      //  if (colReport.gameObject.tag == "villan")
+        //{
+          //  Destroy(other.gameObject);
+        //}
+    //}
+
+  void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "villan")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
